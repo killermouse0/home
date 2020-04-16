5be95c38bb46ad14a0bb20c8fa768615f2fa3243
Stephane KATTOOR <stephane.kattoor@sakana.fr>
2020-04-17 00:24:19 +0200

    General improvement of the script
    * Better error handling
    * Based on the pdf files instead of the torrent files

--- Scripts/sortEBooks.sh
+++ Scripts/sortEBooks.sh
@@ -3,8 +3,26 @@
 # Orders my HumbleBundle eBook bundles to import in Calibre
 # Put them all in a folder and run this without arguments
 
-for i in `echo *.torrent | cut -d: -f1 | sort -u`
-do
-	mkdir $i
-	mv $i.* $i
-done
+DIR="/mnt/c/Users/kille/Downloads/ebooks"
+EXT="pdf"
+
+if [ -n "$1" ]
+then
+	DIR=$1
+fi
+
+echo "Going to process ebooks from $DIR"
+
+if cd $DIR > /dev/null 2>&1
+then
+	for i in `echo *.$EXT`
+	do
+		name=`echo $i | cut -d. -f1 | sort -u`
+		echo Processing $name...
+		mkdir $name
+		mv $name.* $name
+	done
+else
+	echo "$DIR is either missing or not a directory"
+	exit 1
+fi
